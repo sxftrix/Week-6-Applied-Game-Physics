1339aafceeac563a03b4b4384f5d01feb2860289
sxftrix <sxftrix@gmail.com>
2026-02-10 22:36:32 +0800

    updated

--- Week 6/Assets/Scripts/Movement.cs
+++ Week 6/Assets/Scripts/Movement.cs
@@ -1,22 +1,35 @@
-using System.Security.Principal;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
-    [SerializeField] public float speed = 2f;
+    public float speed = 4f;
 
     private Animator animator;
+    private CharacterController controller;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var velocity = Vector3.forward * Input.GetAxis("Vertical") * speed;
-        transform.Translate(velocity * Time.deltaTime);
-        animator.SetFloat("speed", velocity.magnitude);
+        if (!controller || !controller.enabled) return;
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v);
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        controller.SimpleMove(move * speed);
+
+        animator.SetFloat("MoveX", h);
+        animator.SetFloat("MoveZ", v);
+        animator.SetFloat("Speed", move.magnitude);
     }
 }
